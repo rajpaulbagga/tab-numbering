4745d1b22a9daa081b6e01c2a5ef9f59a5015480
Rajpaul Bagga <rbagga@cvent.com>
2020-10-19 12:04:35 -0500

    extract tab handling to method so that update-one tab can work more efficiently

--- tab-numbering.js
+++ tab-numbering.js
@@ -11,8 +11,7 @@ const browser = window.browser || window.chrome;
 const MAX_COUNT = 8; // Max tab that can be accessed this way, apart from special 9 handling as last tab.
 
 const numbers = ['¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
-const number1CodePoint = numbers[0].codePointAt(0);
-const number9CodePoint = numbers[numbers.length - 1].codePointAt(0);
+const numberSet = new Set(numbers)
 
 /*
  * Function:     update
@@ -26,62 +25,60 @@ const update = visibleTabs => {
 
   for (var i = 0; i < visibleTabs.length; i++) {
     let tab = visibleTabs[i];
-
-    const oldTitle = tab.title;
-    let newTitle = oldTitle;
-
-    if (!newTitle) {
-      console.log('missing title. Bail');
-      return;
-    }
-
-
-    // Take out one of these numbers if it already exists in the title
-    if (i >= MAX_COUNT && titleContainsIndexIndicator(newTitle)) {
-      console.log('stripping number from title outside of range');
-      newTitle = newTitle.substring(1);
-    }
-
-    if (i < MAX_COUNT) {
-      if (numbers[i] === newTitle[0]) {
-        console.log("current title correct: ", oldTitle, '/', newTitle)
-        continue;
-      }
-      else if (titleContainsIndexIndicator(newTitle)) {
-        console.log("current title is numbered, but wrong");
-        newTitle = numbers[i] + newTitle.substring(1);
-      }
-      else {
-        console.log("current title is not numbered but needs to be");
-        newTitle = numbers[i] + newTitle;
-      }
-    }
-    if (i >= MAX_COUNT && i == visibleTabs.length - 1) {
-      // Special last tab handling as '9'
-      if (newTitle[0] != numbers[MAX_COUNT]) {
-        newTitle = numbers[MAX_COUNT] + newTitle;
-      }
-    }
-    console.log('  oldTitle: ', oldTitle, '; newTitle: ', newTitle);
-    if (oldTitle !== newTitle) {
-      try {
-        browser.tabs.executeScript(
-          tab.id,
-          {
-            code: `document.title = ${JSON.stringify(newTitle)};`
-          }
-        );
-        console.log(`Executed: ${tab.id}`);
-      } catch (e) {
-        console.log('Tab numbering error:', e);
-      }
-    }
+    updateTab(tab, i, visibleTabs.length);
   }
 };
 
-function titleContainsIndexIndicator(title) {
-  let firstCharCodePoint = title.codePointAt(0);
-  return firstCharCodePoint >= number1CodePoint && firstCharCodePoint <= number9CodePoint;
+function updateTab(tab, tabIndex, visibleTabCount) {
+  const oldTitle = tab.title;
+  let newTitle = oldTitle;
+
+  if (!newTitle) {
+    console.log('missing title. Bail');
+    return;
+  }
+
+
+  // Take out one of these numbers if it already exists in the title
+  if (tabIndex >= MAX_COUNT && numberSet.has(newTitle[0])) {
+    console.log('stripping number from title outside of range');
+    newTitle = newTitle.substring(1);
+  }
+
+  if (tabIndex < MAX_COUNT) {
+    if (numbers[tabIndex] === newTitle[0]) {
+      console.log("current title correct: ", oldTitle, '/', newTitle)
+      return;
+    }
+    else if (numberSet.has(newTitle[0])) {
+      console.log("current title is numbered, but wrong");
+      newTitle = numbers[tabIndex] + newTitle.substring(1);
+    }
+    else {
+      console.log("current title is not numbered but needs to be");
+      newTitle = numbers[tabIndex] + newTitle;
+    }
+  }
+  if (tabIndex >= MAX_COUNT && tabIndex == visibleTabCount - 1) {
+    // Special last tab handling as '9'
+    if (newTitle[0] != numbers[MAX_COUNT]) {
+      newTitle = numbers[MAX_COUNT] + newTitle;
+    }
+  }
+  console.log('  oldTitle: ', oldTitle, '; newTitle: ', newTitle);
+  if (oldTitle !== newTitle) {
+    try {
+      browser.tabs.executeScript(
+        tab.id,
+        {
+          code: `document.title = ${JSON.stringify(newTitle)};`
+        }
+      );
+      console.log(`Executed: ${tab.id}`);
+    } catch (e) {
+      console.log('Tab numbering error:', e);
+    }
+  }
 }
 
 function onError(error) {
@@ -126,9 +123,20 @@ browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
 
 // Must listen for tab updates
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  //update(tab);
-  console.log('update one tab!');
-  updateAll();
+  let querying = browser.tabs.query({ hidden: false, currentWindow: true });
+  let visibleTabCount = 0;
+  let tabIndex = 0;
+  querying.then(tabs => {
+    visibleTabCount = tabs.length;
+    for (var i = 0; i < visibleTabCount; i++) {
+      if (tabs[i].id == tabId) {
+        tabIndex = i;
+        break;
+      }
+    }
+    console.log('update one tab!', tabIndex, visibleTabCount);
+    updateTab(tab, tabIndex, visibleTabCount);
+    }, onError)
 });
 console.log('startup!');
 updateAll();
